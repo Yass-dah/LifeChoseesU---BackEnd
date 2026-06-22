a42b4086002fbe36781d5e6323d8856573fef4b9
Yassir Dahraoui <dahraoui.yassir@educ.di.unito.it>
2026-06-22 03:53:43 +0200

    redefined loginfilter

--- src/main/java/lcu/backend/server/filter/LoginFilter.java
+++ src/main/java/lcu/backend/server/filter/LoginFilter.java
@@ -5,11 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collection;
 
 @Component
 @Order(1)
@@ -27,32 +25,17 @@ public class LoginFilter implements Filter {
             return;
         }
         String path = request.getServletPath();
+        if (path.startsWith("/session") || path.equals("/help-requests/countries")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
         HttpSession session = request.getSession(false);
         String user = session != null ? (String) session.getAttribute("username") : null;
-        if (path.startsWith("/session") || path.equals("/help-requests/countries") || user != null) {
+        if (user != null) {
             filterChain.doFilter(servletRequest, servletResponse);
-            appendPartitionedCookie(response);
             return;
         }
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-    }
-
-    private void appendPartitionedCookie(HttpServletResponse response) {
-        Collection<String> cookieHeaders = response.getHeaders(HttpHeaders.SET_COOKIE);
-        if (cookieHeaders != null && !cookieHeaders.isEmpty()) {
-            boolean first = true;
-            for (String cookie : cookieHeaders) {
-                if (cookie.contains("LCU_SESSION_COOKIE") && !cookie.contains("Partitioned")) {
-                    if (!cookie.contains("SameSite"))
-                        cookie += "; SameSite=None";
-                    cookie += "; Partitioned";
-                }
-
-                if (first) {
-                    response.setHeader(HttpHeaders.SET_COOKIE, cookie);
-                    first = false;
-                } else response.addHeader(HttpHeaders.SET_COOKIE, cookie);
-            }
-        }
     }
 }
